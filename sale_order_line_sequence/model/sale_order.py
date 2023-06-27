d9f0ea57fea1b2196c0955b9ced45ae52f686130
OriolMForgeFlow <oriol.miranda@forgeflow.com>
2023-06-27 08:34:18 +0200

    [IMP] sale_order_line_sequence: use correct sequence in moves when section or notes in sale order

--- sale_order_line_sequence/model/sale_order.py
+++ sale_order_line_sequence/model/sale_order.py
@@ -31,9 +31,32 @@ class SaleOrder(models.Model):
                     line.sequence = current_sequence
                 current_sequence += 1
 
+    def _update_moves_sequence(self):
+        for order in self:
+            if any(
+                [
+                    ptype in ["product", "consu"]
+                    for ptype in order.order_line.mapped("product_id.type")
+                ]
+            ):
+                pickings = order.picking_ids.filtered(
+                    lambda x: x.state not in ("done", "cancel")
+                )
+                if pickings:
+                    picking = pickings[0]
+                    order_lines = order.order_line.filtered(
+                        lambda l: l.product_id.type in ["product", "consu"]
+                    )
+                    for move, line in zip(
+                        sorted(picking.move_lines, key=lambda m: m.id), order_lines
+                    ):
+                        move.write({"sequence": line.sequence})
+
     def write(self, line_values):
         res = super(SaleOrder, self).write(line_values)
         self._reset_sequence()
+        if "order_line" in line_values:
+            self._update_moves_sequence()
         return res
 
     def copy(self, default=None):
