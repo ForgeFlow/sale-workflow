754663f3a1d12fb2b3a18b320573c58d935b3b45
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2025-12-10 18:04:15 +0100

    [IMP] sale_require_po_doc: Set default customer need po at company level

---
+++ sale_require_po_doc/models/res_company.py
@@ -0,0 +1,12 @@
+from odoo import fields, models
+
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    customer_need_po_default = fields.Boolean(
+        string="Default: Customer Requires PO",
+        help="If True, new customers will have 'Customer Requires PO' "
+        "enabled by default."
+    )
